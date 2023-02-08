63d74cc1924a72c7d035af144f06eb051b966331
André Negrelli <65674550+AndreNegrelli@users.noreply.github.com>
2023-02-08 14:40:05 -0300

    6

--- API-vendas/src/shared/http/routes/index.ts
+++ API-vendas/src/shared/http/routes/index.ts
@@ -1,5 +1,6 @@
 
 import productRouter from "@modules/products/routes/products.routes"
+import passwordRouter from "@modules/users/routes/password.router"
 import sessionRouter from "@modules/users/routes/sessions.routers"
 import userRouter from "@modules/users/routes/users.routes"
 import {Router} from "express"
@@ -9,5 +10,6 @@ const routes = Router()
 routes.use('/products', productRouter)
 routes.use('/users', userRouter)
 routes.use('/sessions', sessionRouter)
+routes.use('/password', passwordRouter)
 
 export default routes
